cddeb23c33a18a78c70c0281ef48b501c0fc6245
Yosefgli <ygli770@gmail.com>
2025-06-19 23:43:24 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,29 @@
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('pwa-cache').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json',
-        './icon.png',
-        'https://unpkg.com/html5-qrcode'
-      ]);
-    })
-  );
+// service-worker.js
+
+self.addEventListener("install", function (event) {
+  // מיידי – לא מחכה לגרסה קודמת
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("activate", function (event) {
+  // מוחק את כל הקאש
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function (event) {
+  // תמיד יביא מהשרת – בלי cache
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    fetch(event.request, { cache: "no-store" }).catch(function () {
+      return new Response("Offline", { status: 503, statusText: "Offline" });
     })
   );
 });
